4f6fc8fd5a57dbcb6c427da2a43c86579f5b3b00
AShakulia <100370039+AShakulia@users.noreply.github.com>
2025-03-05 13:59:10 +0300

    fix-header

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -5,7 +5,6 @@
     </Notivue>
     <SideMenu v-if="route.meta.showSidebar" />
     <div class="content-wrapper" :class="{ 'content-wrapper--expanded': !SidebarIsOpen }">
-      <Header class="header" v-if="route.meta.showHeader"/>
       <main class="main-content">
         <slot />
       </main>
@@ -16,7 +15,6 @@
 
 <script setup lang="ts">
 import SideMenu from '@/components/layout/SideMenu.vue';
-import Header from '@/components/layout/Header.vue';
 import Footer from '@/components/layout/Footer.vue';
 import { useLayoutStore } from '@/stores/useLayoutStore';
 import { storeToRefs } from 'pinia';
@@ -47,7 +45,6 @@ const { SidebarIsOpen } = storeToRefs(layoutStore);
   }
 }
 
-.header,
 .footer {
   width: 100%;
   max-width: 48rem;
@@ -58,7 +55,6 @@ const { SidebarIsOpen } = storeToRefs(layoutStore);
   flex-grow: 1;
   display: flex;
   justify-content: center;
-  align-items: center;
   width: 48rem;
 }
 
