75938c537a660e7ac79850615b8a62d75347e1af
teraptor <bat.ggl@gmail.com>
2025-04-17 09:38:59 +0300

    my

--- src/plugins/notifications.ts
+++ src/plugins/notifications.ts
@@ -1,12 +1,8 @@
 import { push } from 'notivue'
 
-interface NotificationOptions {
-    timeout: number;
-}
-
 export const notifications = {
-    success: (message: string, options: NotificationOptions = { timeout: 5000 }) => push.success(message, options),
-    error: (message: string, options: NotificationOptions = { timeout: 5000 }) => push.error(message, options),
-    warning: (message: string, options: NotificationOptions = { timeout: 5000 }) => push.warning(message, options),
-    info: (message: string, options: NotificationOptions = { timeout: 5000 }) => push.info(message, options)
+    success: (message: string) => push.success({ message, duration: 2000 }),
+    error: (message: string) => push.error({ message, duration: 2000 }),
+    warning: (message: string) => push.warning({ message, duration: 2000 }),
+    info: (message: string) => push.info({ message, duration: 2000 })
 }
